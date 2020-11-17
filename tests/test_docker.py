018c088653b327d751620e47ab8202c9023c83ba
Martin Sjölund <martin.sjolund@liu.se>
2020-11-17 10:43:31 +0100

    Use 1.16.1 and test port=11111

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -4,10 +4,13 @@ import tempfile, shutil, os
 
 class DockerTester(unittest.TestCase):
   def testDocker(self):
-    om = OMPython.OMCSessionZMQ(docker="openmodelica/openmodelica:v1.16.0-minimal")
-    assert(om.sendExpression("getVersion()") == "OpenModelica 1.16.0")
+    om = OMPython.OMCSessionZMQ(docker="openmodelica/openmodelica:v1.16.1-minimal")
+    assert(om.sendExpression("getVersion()") == "OpenModelica 1.16.1")
     omInner = OMPython.OMCSessionZMQ(dockerContainer=om._dockerCid)
-    assert(omInner.sendExpression("getVersion()") == "OpenModelica 1.16.0")
+    assert(omInner.sendExpression("getVersion()") == "OpenModelica 1.16.1")
+    om2 = OMPython.OMCSessionZMQ(docker="openmodelica/openmodelica:v1.16.1-minimal", port=11111)
+    assert(om2.sendExpression("getVersion()") == "OpenModelica 1.16.1")
+    del(om2)
     del(omInner)
     del(om)
 if __name__ == '__main__':
