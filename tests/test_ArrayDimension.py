a44f0ab364cb5769614c4c9ede626079d6031b20
arun3688 <rain100falls@gmail.com>
2024-12-06 11:44:29 +0100

    allow ident's in array dimension

--- tests/test_ArrayDimension.py
+++ tests/test_ArrayDimension.py
@@ -26,6 +26,12 @@ class Test_ArrayDimension:
         result = omc.sendExpression("getComponents(A)")
         assert result[0][-1] == (6,7), f"array dimension does not match the expected value. Got: {result[0][-1]}, Expected: {(6, 7)}"
 
+        omc.sendExpression("loadString(\"model A Integer y = 5; Integer x[y+1,1+8]; end A;\")")
+        omc.sendExpression("getErrorString()")
+
+        result = omc.sendExpression("getComponents(A)")
+        assert result[-1][-1] == ('y+1',9), f"array dimension does not match the expected value. Got: {result[-1][-1]}, Expected: {('y+1', 9)}"
+
         omc.__del__()
         shutil.rmtree(tempdir, ignore_errors= True)
 
