53fc10ded9a89d33238c529352bc01f989a80e80
Ondřej Sluka <ondras12345@gmail.com>
2025-04-07 11:13:41 +0200

    Add test for ModelicaSystem.setParameters()
    This test is currently failing, because getParameters() returns str
    instead of float. However, the documentation
    https://openmodelica.org/doc/OpenModelicaUsersGuide/latest/ompython.html#simulation
    says it should return floats.

--- tests/test_ModelicaSystem.py
+++ tests/test_ModelicaSystem.py
@@ -30,6 +30,39 @@ end M;
             for _ in range(10):
                 worker()
 
+    def test_setParameters(self):
+        omc = OMPython.OMCSessionZMQ()
+        model_path = omc.sendExpression("getInstallationDirectoryPath()") + "/share/doc/omc/testmodels/"
+        mod = OMPython.ModelicaSystem(model_path + "BouncingBall.mo", "BouncingBall", raiseerrors=True)
+
+        # method 1
+        mod.setParameters("e=1.234")
+        mod.setParameters("g=321.0")
+        assert mod.getParameters("e") == [1.234]
+        assert mod.getParameters("g") == [321.0]
+        assert mod.getParameters() == {
+            "e": 1.234,
+            "g": 321.0,
+        }
+
+        # method 2
+        mod.setParameters(["e=21.3", "g=0.12"])
+        assert mod.getParameters() == {
+            "e": 21.3,
+            "g": 0.12,
+        }
+        assert mod.getParameters(["e", "g"]) == [21.3, 0.12]
+        assert mod.getParameters(["g", "e"]) == [0.12, 21.3]
+
+        # method 3
+        mod.setParameters({
+            "e": 2.13,
+            "g": 0.21,
+        })
+        assert mod.getParameters() == {
+            "e": 2.13,
+            "g": 0.21,
+        }
 
 if __name__ == '__main__':
     unittest.main()
