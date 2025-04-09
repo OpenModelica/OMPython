831a7d8ffefd28ea4e1d80de85ed2755144f6cf2
Ondřej Sluka <ondras12345@gmail.com>
2025-04-09 19:05:34 +0200

    Use python3 features to clean up ModelicaSystem
    - use f-strings to make sendExpression() arguments easier to read,
    - use pathlib.Path to convert \ to / in file paths on Windows
    - fix a few bugs (mostly missing spaces in error strings)

--- tests/test_ModelicaSystem.py
+++ tests/test_ModelicaSystem.py
@@ -4,15 +4,16 @@ import tempfile
 import shutil
 import os
 import pathlib
+import numpy as np
 
 
 class ModelicaSystemTester(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(ModelicaSystemTester, self).__init__(*args, **kwargs)
-        self.tmp = tempfile.mkdtemp(prefix='tmpOMPython.tests')
-        with open("%s/M.mo" % self.tmp, "w") as fout:
+        self.tmp = pathlib.Path(tempfile.mkdtemp(prefix='tmpOMPython.tests'))
+        with open(self.tmp / "M.mo", "w") as fout:
             fout.write("""model M
-  Real x(start = 1);
+  Real x(start = 1, fixed = true);
   parameter Real a = -1;
 equation
   der(x) = x*a;
@@ -24,7 +25,7 @@ end M;
 
     def testModelicaSystemLoop(self):
         def worker():
-            filePath = os.path.join(self.tmp, "M.mo").replace("\\", "/")
+            filePath = (self.tmp / "M.mo").as_posix()
             m = OMPython.ModelicaSystem(filePath, "M")
             m.simulate()
             m.convertMo2Fmu(fmuType="me")
@@ -79,16 +80,10 @@ end M;
 
     def test_relative_path(self):
         cwd = pathlib.Path.cwd()
-        (fd, name) = tempfile.mkstemp(dir=cwd, text=True)
+        (fd, name) = tempfile.mkstemp(prefix='tmpOMPython.tests', dir=cwd, text=True)
         try:
             with os.fdopen(fd, 'w') as f:
-                f.write("""model M
-  Real x(start = 1, fixed=true);
-  parameter Real a = -1;
-equation
-  der(x) = x*a;
-end M;
-""")
+                f.write((self.tmp / "M.mo").read_text())
 
             model_file = pathlib.Path(name).relative_to(cwd)
             model_relative = str(model_file)
@@ -99,6 +94,43 @@ end M;
         finally:
             # clean up the temporary file
             model_file.unlink()
+
+    def test_customBuildDirectory(self):
+        filePath = (self.tmp / "M.mo").as_posix()
+        tmpdir = self.tmp / "tmpdir1"
+        tmpdir.mkdir()
+        m = OMPython.ModelicaSystem(filePath, "M", raiseerrors=True,
+                                    customBuildDirectory=tmpdir)
+        assert pathlib.Path(m.getWorkDirectory()).resolve() == tmpdir.resolve()
+        result_file = tmpdir / "a.mat"
+        assert not result_file.exists()
+        m.simulate(resultfile="a.mat")
+        assert result_file.is_file()
+
+    def test_getSolutions(self):
+        filePath = (self.tmp / "M.mo").as_posix()
+        mod = OMPython.ModelicaSystem(filePath, "M", raiseerrors=True)
+        x0 = 1
+        a = -1
+        tau = -1 / a
+        stopTime=5*tau
+        mod.setSimulationOptions([f"stopTime={stopTime}", "stepSize=0.1", "tolerance=1e-8"])
+        mod.simulate()
+
+        x = mod.getSolutions("x")
+        t, x2 = mod.getSolutions(["time", "x"])
+        assert (x2 == x).all()
+        sol_names = mod.getSolutions()
+        assert isinstance(sol_names, tuple)
+        assert "time" in sol_names
+        assert "x" in sol_names
+        assert "der(x)" in sol_names
+        with self.assertRaises(OMPython.ModelicaSystemError):
+            mod.getSolutions("t")  # variable 't' does not exist
+        assert np.isclose(t[0], 0), "time does not start at 0"
+        assert np.isclose(t[-1], stopTime), "time does not end at stopTime"
+        x_analytical = x0 * np.exp(a*t)
+        assert np.isclose(x, x_analytical, rtol=1e-4).all()
 
 
 if __name__ == '__main__':
