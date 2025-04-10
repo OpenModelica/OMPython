f5d07e6a4eff2daf2c7d5e7b2d5e6edf9cb75f05
Ondřej Sluka <ondras12345@gmail.com>
2025-04-10 13:07:09 +0200

    Add more tests for ModelicaSystem methods

--- tests/test_ModelicaSystem.py
+++ tests/test_ModelicaSystem.py
@@ -113,7 +113,7 @@ end M;
         x0 = 1
         a = -1
         tau = -1 / a
-        stopTime=5*tau
+        stopTime = 5*tau
         mod.setSimulationOptions([f"stopTime={stopTime}", "stepSize=0.1", "tolerance=1e-8"])
         mod.simulate()
 
@@ -131,6 +131,187 @@ end M;
         assert np.isclose(t[-1], stopTime), "time does not end at stopTime"
         x_analytical = x0 * np.exp(a*t)
         assert np.isclose(x, x_analytical, rtol=1e-4).all()
+
+    def test_getters(self):
+        model_file = self.tmp / "M_getters.mo"
+        model_file.write_text("""
+model M_getters
+  Real x(start = 1, fixed = true);
+  output Real y "the derivative";
+  parameter Real a = -0.5;
+  parameter Real b = 0.1;
+equation
+  der(x) = x*a + b;
+  y = der(x);
+end M_getters;
+""")
+        mod = OMPython.ModelicaSystem(model_file.as_posix(), "M_getters", raiseerrors=True)
+
+        q = mod.getQuantities()
+        assert isinstance(q, list)
+        assert sorted(q, key=lambda d: d["name"]) == sorted([
+            {
+                'alias': 'noAlias',
+                'aliasvariable': None,
+                'causality': 'local',
+                'changeable': 'true',
+                'description': None,
+                'max': None,
+                'min': None,
+                'name': 'x',
+                'start': '1.0',
+                'unit': None,
+                'variability': 'continuous',
+            },
+            {
+                'alias': 'noAlias',
+                'aliasvariable': None,
+                'causality': 'local',
+                'changeable': 'false',
+                'description': None,
+                'max': None,
+                'min': None,
+                'name': 'der(x)',
+                'start': None,
+                'unit': None,
+                'variability': 'continuous',
+            },
+            {
+                'alias': 'noAlias',
+                'aliasvariable': None,
+                'causality': 'output',
+                'changeable': 'false',
+                'description': 'the derivative',
+                'max': None,
+                'min': None,
+                'name': 'y',
+                'start': '-0.4',
+                'unit': None,
+                'variability': 'continuous',
+            },
+            {
+                'alias': 'noAlias',
+                'aliasvariable': None,
+                'causality': 'parameter',
+                'changeable': 'true',
+                'description': None,
+                'max': None,
+                'min': None,
+                'name': 'a',
+                'start': '-0.5',
+                'unit': None,
+                'variability': 'parameter',
+            },
+            {
+                'alias': 'noAlias',
+                'aliasvariable': None,
+                'causality': 'parameter',
+                'changeable': 'true',
+                'description': None,
+                'max': None,
+                'min': None,
+                'name': 'b',
+                'start': '0.1',
+                'unit': None,
+                'variability': 'parameter',
+            }
+        ], key=lambda d: d["name"])
+
+        assert mod.getQuantities("y") == [
+            {
+                'alias': 'noAlias',
+                'aliasvariable': None,
+                'causality': 'output',
+                'changeable': 'false',
+                'description': 'the derivative',
+                'max': None,
+                'min': None,
+                'name': 'y',
+                'start': '-0.4',
+                'unit': None,
+                'variability': 'continuous',
+            }
+        ]
+
+        assert mod.getQuantities(["y", "x"]) == [
+            {
+                'alias': 'noAlias',
+                'aliasvariable': None,
+                'causality': 'output',
+                'changeable': 'false',
+                'description': 'the derivative',
+                'max': None,
+                'min': None,
+                'name': 'y',
+                'start': '-0.4',
+                'unit': None,
+                'variability': 'continuous',
+            },
+            {
+                'alias': 'noAlias',
+                'aliasvariable': None,
+                'causality': 'local',
+                'changeable': 'true',
+                'description': None,
+                'max': None,
+                'min': None,
+                'name': 'x',
+                'start': '1.0',
+                'unit': None,
+                'variability': 'continuous',
+            },
+        ]
+
+        assert mod.getContinuous() == {
+            'x': '1.0',
+            'der(x)': None,
+            'y': '-0.4'
+        }
+        assert mod.getContinuous("y") == ['-0.4']
+        assert mod.getContinuous(["y", "x"]) == ['-0.4', '1.0']
+
+        assert mod.getInputs() == {}
+        assert mod.getOutputs() == {'y': '-0.4'}
+        assert mod.getOutputs("y") == ["-0.4"]
+
+    def test_simulate_inputs(self):
+        model_file = self.tmp / "M_input.mo"
+        model_file.write_text("""
+model M_input
+  Real x(start=0, fixed=true);
+  input Real u;
+  output Real y;
+equation
+  der(x) = u;
+  y = x;
+end M_input;
+""")
+        mod = OMPython.ModelicaSystem(model_file.as_posix(), "M_input", raiseerrors=True)
+
+        mod.setSimulationOptions("stopTime=1.0")
+
+        # integrate a constant
+        mod.setInputs("u=2.5")
+        assert mod.getInputs() == {
+            "u": [
+                (0.0, 2.5),
+                (1.0, 2.5),
+            ],
+        }
+        mod.simulate()
+        y = mod.getSolutions("y")[0]
+        assert np.isclose(y[-1], 2.5)
+
+        # now let's integrate the sum of two ramps
+        mod.setInputs("u=[(0.0, 0.0), (0.5, 2), (1.0, 0)]")
+        assert mod.getInputs("u") == [[
+            (0.0, 0.0),
+            (0.5, 2.0),
+            (1.0, 0.0),
+        ]]
+        mod.simulate()
+        y = mod.getSolutions("y")[0]
+        assert np.isclose(y[-1], 1.0)
 
 
 if __name__ == '__main__':
