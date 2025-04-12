aa8c118f7fa45199899a6fa4e28e41cade8b95ff
Ondřej Sluka <ondras12345@gmail.com>
2025-04-12 15:49:54 +0200

    Fix two spaces in simflags causing error

--- OMPython/ModelicaSystem.py
+++ OMPython/ModelicaSystem.py
@@ -589,7 +589,7 @@ class ModelicaSystem:
             raise Exception(f"Error: Application file path not found: {exe_file}")
 
         cmd = exe_file.as_posix() + override + csvinput + r + simflags
-        cmd = cmd.split(" ")
+        cmd = [s for s in cmd.split(' ') if s]
         self._run_cmd(cmd=cmd)
         self.simulationFlag = True
 
@@ -1018,7 +1018,7 @@ class ModelicaSystem:
             raise Exception(f"Error: Application file path not found: {exe_file}")
         else:
             cmd = exe_file.as_posix() + linruntime + override + csvinput + simflags
-            cmd = cmd.split(' ')
+            cmd = [s for s in cmd.split(' ') if s]
             self._run_cmd(cmd=cmd)
 
         # code to get the matrix and linear inputs, outputs and states
