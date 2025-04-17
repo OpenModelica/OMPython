b17b49fee39772cf2607a5e39497ea30d7c6ec7d
syntron <syntron@web.de>
2025-04-17 19:04:47 +0200

    fix OMCSessionBase - the variables self.readonly and self.omc_cache are needed here

--- OMPython/__init__.py
+++ OMPython/__init__.py
@@ -98,6 +98,10 @@ class DummyPopen():
 
 class OMCSessionBase(metaclass=abc.ABCMeta):
 
+    def __init__(self, readonly=False):
+        self._readonly = readonly
+        self._omc_cache = {}
+
     def clearOMParserResult(self):
         OMParser.result = {}
 
@@ -125,10 +129,10 @@ class OMCSessionBase(metaclass=abc.ABCMeta):
     def ask(self, question, opt=None, parsed=True):
         p = (question, opt, parsed)
 
-        if self.readonly and question != 'getErrorString':
+        if self._readonly and question != 'getErrorString':
             # can use cache if readonly
-            if p in self.omc_cache:
-                return self.omc_cache[p]
+            if p in self._omc_cache:
+                return self._omc_cache[p]
 
         if opt:
             expression = f'{question}({opt})'
@@ -144,7 +148,7 @@ class OMCSessionBase(metaclass=abc.ABCMeta):
             raise
 
         # save response
-        self.omc_cache[p] = res
+        self._omc_cache[p] = res
 
         return res
 
@@ -329,10 +333,10 @@ class OMCSessionZMQ(OMCSessionBase):
         if dockerExtraArgs is None:
             dockerExtraArgs = []
 
+        super().__init__(readonly=readonly)
+
         self.omhome = self._get_omhome(omhome=omhome)
 
-        self.readonly = readonly
-        self.omc_cache = {}
         self._omc_process = None
         self._omc_command = None
         self._omc = None
