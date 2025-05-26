ce8c76d3290f9ecbf6104d8d1e29ebafe4b2c828
syntron <syntron@web.de>
2025-05-26 22:14:54 +0200

    [OMCSessionZMQDocker] add new class to use OMC in docker

--- OMPython/__init__.py
+++ OMPython/__init__.py
@@ -37,7 +37,7 @@ __license__ = """
 """
 
 from OMPython.ModelicaSystem import LinearizationResult, ModelicaSystem, ModelicaSystemCmd, ModelicaSystemError
-from OMPython.OMCSession import OMCSessionCmd, OMCSessionException, OMCSessionZMQ
+from OMPython.OMCSession import OMCSessionCmd, OMCSessionException, OMCSessionZMQ, OMCSessionZMQDocker
 
 # global names imported if import 'from OMPython import *' is used
 __all__ = [
@@ -49,4 +49,5 @@ __all__ = [
     'OMCSessionCmd',
     'OMCSessionException',
     'OMCSessionZMQ',
+    'OMCSessionZMQDocker',
 ]
