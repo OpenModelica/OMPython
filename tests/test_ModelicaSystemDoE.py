02823ca5550ca8bdb89849da7470c0c6d9f2fc22
syntron <syntron@web.de>
2025-06-25 21:07:14 +0200

    [test_ModelicaSystemDoE] update test

--- tests/test_ModelicaSystemDoE.py
+++ tests/test_ModelicaSystemDoE.py
@@ -1,3 +1,4 @@
+import numpy as np
 import OMPython
 import pandas as pd
 import pathlib
@@ -37,7 +38,6 @@ def param_doe() -> dict[str, list]:
 
 
 def test_ModelicaSystemDoE(tmp_path, model_doe, param_doe):
-
     tmpdir = tmp_path / 'DoE'
     tmpdir.mkdir(exist_ok=True)
 
@@ -46,13 +46,32 @@ def test_ModelicaSystemDoE(tmp_path, model_doe, param_doe):
         modelName="M",
         parameters=param_doe,
         resultpath=tmpdir,
+        simargs={"override": {'stopTime': 1.0}},
     )
     mod_doe.prepare()
     df_doe = mod_doe.get_doe()
     assert isinstance(df_doe, pd.DataFrame)
     assert df_doe.shape[0] == 16
-    assert df_doe['results available'].sum() == 16
+    assert df_doe['results available'].sum() == 0
 
     mod_doe.simulate()
-    sol = mod_doe.get_solutions(var_list=['x[1]', 'y[1]'])
-    assert len(sol) == 16
+    assert df_doe['results available'].sum() == 16
+
+    for row in df_doe.to_dict('records'):
+        resultfilename = row[mod_doe.DF_COLUMNS_RESULTFILENAME]
+        resultfile = mod_doe._resultpath / resultfilename
+
+        var_dict = {
+            # simple / non-structural parameters
+            'a': float(row['a']),
+            'b': float(row['b']),
+            # structural parameters
+            'p': float(row['p']),
+            'q': float(row['q']),
+            # variables using the structural parameters
+            f"x[{row['p']}]": float(row['a']),
+            f"y[{row['p']}]": float(row['b']),
+        }
+        sol = mod_doe._mod.getSolutions(resultfile=resultfile.as_posix(), varList=list(var_dict.keys()))
+
+        assert np.isclose(sol[:, -1], np.array(list(var_dict.values()))).all()
