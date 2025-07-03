5b3543866fb22e437ff56e8afdb1286db9f06c5a
syntron <syntron@web.de>
2025-07-03 09:21:35 +0200

    [test_OMCPath] define test using OMCSessionZMQ() locally

--- tests/test_OMCPath.py
+++ tests/test_OMCPath.py
@@ -1,6 +1,8 @@
 import OMPython
+import pytest
 
 
+@pytest.mark.skip(reason="This test would fail (no docker on github)")
 def test_OMCPath_docker():
     omcp = OMPython.OMCProcessDocker(docker="openmodelica/openmodelica:v1.25.0-minimal")
     om = OMPython.OMCSessionZMQ(omc_process=omcp)
@@ -18,6 +20,23 @@ def test_OMCPath_docker():
     assert p2.is_file() is False
 
     del omcp
+    del om
+
+
+def test_OMCPath_local():
+    om = OMPython.OMCSessionZMQ()
+
+    p1 = om.omcpath('/tmp')
+    assert str(p1) == "/tmp"
+    p2 = p1 / 'test.txt'
+    assert str(p2) == "/tmp/test.txt"
+    assert p2.write_text('test')
+    assert p2.read_text() == "test"
+    assert p2.is_file()
+    assert p2.parent.is_dir()
+    assert p2.unlink()
+    assert p2.is_file() is False
+
     del om
 
 
