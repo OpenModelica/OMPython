675a2adde03a94bf8856a7d0261a17d53d7ee4ab
syntron <32058823+syntron@users.noreply.github.com>
2025-11-05 12:47:23 +0100

    [OMCSessionRunData] run executable via OMCSessionZMQ (#331)
    * [OMCSessionRunData] add new class to store all information about a model executable
    
    * [OMCSessionRunData] use class to move run of model executable to OMSessionZMQ
    
    * [test_ModelicaSystemCmd] fix test
    
    * [OMCSessionRunData] add to __init__
    
    * [test_ModelicaSystemCmd] fix test (again)

--- tests/test_ModelicaSystemCmd.py
+++ tests/test_ModelicaSystemCmd.py
@@ -18,7 +18,11 @@ end M;
 @pytest.fixture
 def mscmd_firstorder(model_firstorder):
     mod = OMPython.ModelicaSystem(fileName=model_firstorder.as_posix(), modelName="M")
-    mscmd = OMPython.ModelicaSystemCmd(runpath=mod.getWorkDirectory(), modelname=mod._model_name)
+    mscmd = OMPython.ModelicaSystemCmd(
+        session=mod._getconn,
+        runpath=mod.getWorkDirectory(),
+        modelname=mod._model_name,
+    )
     return mscmd
 
 
@@ -32,8 +36,7 @@ def test_simflags(mscmd_firstorder):
     with pytest.deprecated_call():
         mscmd.args_set(args=mscmd.parse_simflags(simflags="-noEventEmit -noRestart -override=a=1,x=3"))
 
-    assert mscmd.get_cmd() == [
-        mscmd.get_exe().as_posix(),
+    assert mscmd.get_cmd_args() == [
         '-noEventEmit',
         '-noRestart',
         '-override=a=1,b=2,x=3',
@@ -43,8 +46,7 @@ def test_simflags(mscmd_firstorder):
         "override": {'b': None},
     })
 
-    assert mscmd.get_cmd() == [
-        mscmd.get_exe().as_posix(),
+    assert mscmd.get_cmd_args() == [
         '-noEventEmit',
         '-noRestart',
         '-override=a=1,x=3',
