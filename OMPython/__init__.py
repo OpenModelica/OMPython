16ea86f3f9231c730436738300368a9a3d79e725
syntron <syntron@web.de>
2026-01-24 21:25:12 +0100

    [ModelicaSystem] rename ModelicaSystemDoE => ModelicaDoEOMC
    * add compatibility variable for ModelicaSystemDoE

--- OMPython/__init__.py
+++ OMPython/__init__.py
@@ -17,6 +17,7 @@ from OMPython.ModelicaSystem import (
     ModelicaSystemOMC,
     ModelExecutionCmd,
     ModelicaSystemDoE,
+    ModelicaDoEOMC,
     ModelicaSystemError,
 )
 from OMPython.OMCSession import (
@@ -47,6 +48,7 @@ __all__ = [
     'ModelicaSystemOMC',
     'ModelExecutionCmd',
     'ModelicaSystemDoE',
+    'ModelicaDoEOMC',
     'ModelicaSystemError',
 
     'OMCPath',
