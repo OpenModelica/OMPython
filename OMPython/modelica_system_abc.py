5dc467d6974039cacf16d1ea9cff5215fbc64b9c
syntron <32058823+syntron@users.noreply.github.com>
2026-08-31 20:17:54 +0200

    (I003) update usage of variable_filter (#464)
    * update handling of variable_filter
    
    * use public function `ModelicaSystemABC.set_variable_filter()` to define
    * process it in `ModelicaSystemABC._process_override_data()` as command line argument
    
    * use new processing for variable_filter
    
    * add unittest test_variable_filter()

--- OMPython/modelica_system_abc.py
+++ OMPython/modelica_system_abc.py
@@ -596,12 +596,20 @@ class ModelicaSystemABC(metaclass=abc.ABCMeta):
             override_file: OMPathABC,
             override_var: dict[str, str],
             override_sim: dict[str, str],
+            variable_filter: Optional[str] = None,
     ) -> None:
         """
         Define the override parameters. As the definition of simulation specific override parameter changes with OM
         1.26.0, version specific code is needed. Please keep in mind, that this will fail if OMC is not used to run the
         model executable.
+
+        Including also override of variable filter settings.
         """
+
+        # define variable filter if defined (override any original setting)
+        if variable_filter is not None:
+            om_cmd.arg_set(key="variableFilter", val=variable_filter)
+
         if len(override_var) == 0 and len(override_sim) == 0:
             return
 
@@ -672,6 +680,7 @@ class ModelicaSystemABC(metaclass=abc.ABCMeta):
             override_file=result_file.parent / f"{result_file.stem}_override.txt",
             override_var=self._override_variables,
             override_sim=self._simulate_options_override,
+            variable_filter=self._variable_filter,
         )
 
         if self._inputs:  # if model has input quantities
@@ -937,6 +946,29 @@ class ModelicaSystemABC(metaclass=abc.ABCMeta):
             datatype="optimization-option",
             overridedata=None)
 
+    def set_variable_filter(
+            self,
+            variable_filter: Optional[str] = None,
+            escape: bool = False,
+    ) -> None:
+        """
+        This method is used to set variable filters. If escape is True, all regex special characters are escaped.
+        """
+        if variable_filter is None:
+            self._variable_filter = None
+            return
+
+        if escape:
+            variable_filter = re.escape(variable_filter)
+
+        # Validate filter_val as a regular expression
+        try:
+            re.compile(variable_filter)
+        except re.error as exc:
+            raise ModelicaSystemError(f"Invalid variable_filter regular expression: {variable_filter!r} ({exc})")
+
+        self._variable_filter = variable_filter
+
     @staticmethod
     def toInputs(data: dict[str, list[float]]) -> dict[str, list[tuple[float, float]]]:
         """
@@ -1158,6 +1190,7 @@ class ModelicaSystemABC(metaclass=abc.ABCMeta):
             override_file=self.getWorkDirectory() / f'{self._model_name}_override_linear.txt',
             override_var=self._override_variables,
             override_sim=self._linearization_options,
+            variable_filter=self._variable_filter,
         )
 
         if self._inputs:
